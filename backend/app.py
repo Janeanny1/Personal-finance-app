41a6cfab56eaa08748849190f198c562b308aa43
Janeanny1 <janethmalikita@gmail.com>
2025-05-24 02:04:53 +0300

    New updates

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 app.register_blueprint(auth_bp)
 app.register_blueprint(trans_bp)
 
