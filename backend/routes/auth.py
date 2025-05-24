72a6437ced31b513c28cb0ef75b6a280b3acbcaa
Janet Anne <110091279+Janeanny1@users.noreply.github.com>
2025-05-24 03:03:42 +0300

    Update auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from db_config import db
+from backend.db_config import db
 from models import User
 
 auth_bp = Blueprint('auth', __name__)
